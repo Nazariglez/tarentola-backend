7ec904a2b1c11e1907ab7a9ac606ca5321d475b6
Nazarí González <nazari.nz@gmail.com>
2017-09-30 18:12:37 +0100

    Batman! (this commit has no parents)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tarentola-backend
 Backend API for the portal tarentola.com
+
+###Requirements 
+Golang 1.9 && Node 8
---
+++ main.go
@@ -0,0 +1,9 @@
+// Created by nazarigonzalez on 30/9/17.
+
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Hello world!!")
+}
