8cbd0a24859509777aa546467dde789990d7d75f
Nazarí González <nazari.nz@gmail.com>
2017-10-18 23:33:34 +0100

    last commits it’s wrong, the description it’s for this. Added an internal ticker to remove expired confirmation tokens.

--- database/usertempmodel/user_temp.go
+++ database/usertempmodel/user_temp.go
@@ -4,8 +4,12 @@ package usertempmodel
 
 import (
 	"github.com/jinzhu/gorm"
+	"github.com/nazariglez/tarentola-backend/config"
 	"github.com/nazariglez/tarentola-backend/database/helpers"
+	"github.com/nazariglez/tarentola-backend/database/usermodel"
+	"github.com/nazariglez/tarentola-backend/logger"
 	"github.com/nazariglez/tarentola-backend/utils"
+	"time"
 )
 
 type UserTemp struct {
@@ -16,10 +20,12 @@ type UserTemp struct {
 }
 
 var db *gorm.DB
+var ticker *time.Ticker
+var expireTime time.Duration
 
 func Init(database *gorm.DB) (interface{}, func() error) {
 	db = database
-	return &UserTemp{}, nil
+	return &UserTemp{}, initTicker
 }
 
 func (u *UserTemp) BeforeCreate(scope *gorm.Scope) error {
@@ -49,4 +55,46 @@ func (u *UserTemp) BeforeCreate(scope *gorm.Scope) error {
 
 	scope.SetColumn("Token", token)
 	return nil
+}
+
+func initTicker() error {
+	expireTime = time.Duration(config.Data.Auth.ConfirmTokenExpire) * time.Hour
+	ticker = time.NewTicker(time.Second * 3600)
+
+	go runTicker()
+	return nil
+}
+
+func runTicker() {
+	for _ = range ticker.C {
+		cleanTempUsers()
+	}
+}
+
+func cleanTempUsers() {
+	logger.Log.Debug("Cleaning temporal users...")
+	users := []UserTemp{}
+	t := time.Now().Add(-expireTime)
+
+	err := db.Where("created_at < ?", t).Find(&users).Error
+	if err != nil && !helpers.IsNotFoundErr(err) {
+		logger.Log.Errorf("%s -> Cleaning temporal users.", err.Error())
+		return
+	}
+
+	if len(users) != 0 {
+		for _, u := range users {
+			if err := DeleteByID(u.ID); err != nil {
+				logger.Log.Errorf("%s -> Cleaning temporal user with id '%d'.", err.Error(), u.ID)
+				continue
+			}
+
+			if err := usermodel.DeleteByID(u.UserID); err != nil {
+				logger.Log.Errorf("%s -> Cleaning tempoaral user with id '%d', and user with id '%d'.", err.Error(), u.ID, u.UserID)
+				continue
+			}
+		}
+	}
+
+	logger.Log.Debugf("Cleaned '%d' inactive temporal users.", len(users))
 }
